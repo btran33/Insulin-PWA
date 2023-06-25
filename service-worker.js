6178b43554ce22896a01fcd3edf2f52cdbf068f2
Ben Tran <btran33@illinois.edu>
2023-06-25 15:27:16 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,51 @@
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-);
+const GHPATH = 'https://btran33.github.io/Insulin-PWA';
+const APP_PREFIX = 'insulin_pwa';
+const VERSION = ':version_0.2.0';
+const URLS = [
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`
+];
 
-workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
-);
+const CACHE_NAME = APP_PREFIX + VERSION;
+self.addEventListener('fetch', function (e) {
+  console.log('Fetch request : ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('Responding with cache : ' + e.request.url);
+        return request;
+      } else {
+        console.log('File is not cached, fetching : ' + e.request.url);
+        return fetch(e.request);
+      }
+    })
+  );
+});
 
-const cacheName = 'v1'
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Installing cache : ' + CACHE_NAME);
+      return cache.addAll(URLS);
+    })
+  );
+});
 
-const cacheClone = async (e) => {
-  const res = await fetch(e.request);
-  const resClone = res.clone();
-
-  const cache = await caches.open(cacheName);
-  await cache.put(e.request, resClone);
-  return res;
-};
-
-const fetchEvent = () => {
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      cacheClone(e)
-        .catch(() => caches.match(e.request))
-        .then((res) => res)
-    );
-  });
-};
-
-fetchEvent();
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            console.log('Deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
